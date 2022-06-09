b7c9ad3cb683b51b534eaa64a0dc3599dcaacc0a
Matvey <Caplinmatvey.12345@gmail.com>
2022-06-09 17:57:54 +0500

    change db

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 
 const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
+    user: "qoveryadmin",
+    host: "zdb1a7152-postgresql.z10146392.xms.sh",
     database: "calldb",
-    password: "ytrewqytrewq",
+    password: "gsXfa1IhtLLn0qWYJWtGG9-oCrtchdbx",
     port: 5432,
 });
 
